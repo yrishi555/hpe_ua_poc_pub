e17351506fbbb1d2540aa8fa95c0f546beaa82c1
joshy.pj@hpe.com <joshy.pj@hpe.com>
2023-11-30 22:18:04 +0530

    dag1

--- Data-Engineering/Airflow/True_Corp_Dag.py
+++ Data-Engineering/Airflow/True_Corp_Dag.py
@@ -29,8 +29,8 @@ dag = DAG(
     }
 )
 
-submit_job = SparkSubmitOperator(
-    task_id='submit_job',
+load_data_job = SparkSubmitOperator(
+    task_id='load_data_job',
     application= '/scripts/hive_dataload.py',
     conn_id = 'spark_con1',
     total_executor_cores='1',
@@ -41,4 +41,16 @@ submit_job = SparkSubmitOperator(
     verbose=False
 )
 
-submit_job 
+check_data_job = SparkSubmitOperator(
+    task_id='check_data_job',
+    application= '/scripts/hive_dataload.py',
+    conn_id = 'spark_con1',
+    total_executor_cores='1',
+    executor_cores='1',
+    executor_memory='2g',
+    num_executors='1',
+    driver_memory='2g',
+    verbose=False
+)
+
+load_data_job >> check_data_job 
