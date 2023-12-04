7f2a58d9aa36b403745a636321949ddd6e0bec48
joshy.pj@hpe.com <joshy.pj@hpe.com>
2023-12-04 14:33:19 +0530

    added files

--- Data-Engineering/Airflow/True_Corp_DAG_1.py
+++ Data-Engineering/Airflow/True_Corp_DAG_1.py
@@ -49,6 +49,7 @@ task2=SparkKubernetesOperator(
     do_xcom_push=True,
     dag=dag,
     api_group="sparkoperator.hpe.com",
+    kubernetes_conn_id='k8s'
     enable_impersonation_from_ldap_user=True
 )
 task3 = PythonOperator(
