8d8ad931ddaef0e00b4c09830babf81c3cc5be3a
joshy.pj@hpe.com <joshy.pj@hpe.com>
2023-12-04 12:05:20 +0530

    added files

--- Data-Engineering/Airflow/True_Corp_DAG_1.py
+++ Data-Engineering/Airflow/True_Corp_DAG_1.py
@@ -1,31 +1,28 @@
+from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.models.param import Param
+
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
-from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(1),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'max_active_runs': 1,
-    'retries': 0
+    'start_date': datetime(2023,7,5),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
-
 dag = DAG(
-    'True_Corp_k8s_DAG_1',
+    'spark-etl-new',
     default_args=default_args,
+    description='Banking-data-demo',
     schedule_interval=None,
-    tags=['e2e example', 'ezaf', 'spark', 'csv', 'parquet', 'fts'],
-    # params={
-    #     'training_path': Param("financial-processed", type="string"),
-    #     's3_secret_name': Param("spark-s3-creds", type="string"),
-    #     'airgap_registry_url': Param("", type=["null", "string"], pattern=r"^$|^\S+/$")
-    # },
-    render_template_as_native_obj=True,
+    tags=['e2e example','ETL', 'spark'],
+    params={
+        'username': Param("hpedemo-user01", type="string"),
+        's3_secret_name': Param("spark-s3-creds", type="string")
+    },
     access_control={
         'All': {
             'can_read',
@@ -35,21 +32,29 @@ dag = DAG(
     }
 )
 
-submit = SparkKubernetesOperator(
-    task_id='submit',
-    application_file="True_hive_to_s3.yaml",
+def start_job():
+    print("Start Data Reading from Minio S3 Bucket and QA1 MySQL Database...")
+
+def end_job():
+    print("Data Loading to S3 Done...")
+
+task1 = PythonOperator(
+    task_id='Start_Data_Reading',
+    python_callable=start_job,
+    dag=dag,
+)
+
+task2=SparkKubernetesOperator(
+    task_id='Spark_etl_submit',
+    application_file="spark_etl_new.yaml",
     do_xcom_push=True,
     dag=dag,
     api_group="sparkoperator.hpe.com",
     enable_impersonation_from_ldap_user=True
 )
-
-sensor = SparkKubernetesSensor(
-    task_id='monitor',
-    application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+task3 = PythonOperator(
+    task_id='Data_Loading_Done',
+    python_callable=end_job,
     dag=dag,
-    api_group="sparkoperator.hpe.com",
-    attach_log=True
 )
-
-submit >> sensor
+task1>>task2>> task3
