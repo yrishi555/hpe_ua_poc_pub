b132707ae39b3a96d8b523c076d11e09dba8697d
joshy.pj@hpe.com <joshy.pj@hpe.com>
2023-12-04 15:10:21 +0530

    added files

--- Data-Engineering/Airflow/True_Corp_DAG_1.py
+++ Data-Engineering/Airflow/True_Corp_DAG_1.py
@@ -9,7 +9,7 @@ from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKube
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023,7,5),
+    'start_date': datetime(2023,12,4),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
