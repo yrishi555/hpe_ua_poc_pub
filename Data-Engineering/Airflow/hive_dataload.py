0702494315f545b0aa1b450e53365311a666b073
joshy.pj@hpe.com <joshy.pj@hpe.com>
2023-12-04 17:25:01 +0530

    added files

--- Data-Engineering/Airflow/hive_dataload.py
+++ Data-Engineering/Airflow/hive_dataload.py
@@ -4,7 +4,7 @@ from py4j.java_gateway import java_import
 java_import(spark._sc._jvm, "org.apache.spark.sql.api.python.*")
 from delta.tables import DeltaTable
 # Define the folder path
-delta_path = "s3a://truepoc-bkt-raw/test/"
+delta_path = "s3a://truepoc-bkt-raw/test123/"
 # Connects to EzPresto over SSL, executes a SQL query, and loads the result into a Spark DataFrame
 df = spark.read.format("jdbc").\
       option("driver", "com.facebook.presto.jdbc.PrestoDriver").\
